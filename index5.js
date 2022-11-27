318b7469de492a3a1c698762dc8099ff8fd49b0a
Viktoryia-Kasabutskaya <viktoryia.kasabutskaya@gmail.com>
2022-11-27 12:59:43 +0100

    added folder

--- index5.js
+++
@@ -1,8 +0,0 @@
-// Ваша цель - создать функцию deleteCharts(str), которая удаляет первый и последний символы строки, которая передается в 
-// параметр, и возвращает новую строку без этих символов.
-
-const deleteCharts = function (str) {
-    return str.slice(1, -1)
-}
-
-console.log(deleteCharts('hello'))
